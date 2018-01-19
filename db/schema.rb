4b752edecad93764b19136741d6efad6ddf4eb35
Finance app <dylangodfrey12@gmail.com>
2018-01-19 15:48:47 +0000

    pass reset added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118164818) do
+ActiveRecord::Schema.define(version: 20180119144739) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20180118164818) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
